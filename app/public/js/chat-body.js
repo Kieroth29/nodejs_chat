cb429381f2cc1ff0e846ee3df8e6db31b211dca3
Marcos Vinícius Queiroz <49909597+Kieroth29@users.noreply.github.com>
2022-08-05 00:16:55 -0300

    Refactoring to follow Content Security Policy directive: "script-src 'self'"

---
+++ app/public/js/chat-body.js
@@ -0,0 +1,74 @@
+var ws = io.connect('https://chat.kieroth29.xyz');
+var socketId;
+var userId;
+
+ws.on('connect', function () {
+    socketId = ws.id;
+
+    $("#users")
+    .append(`
+        <span class="user" id="${socketId}">
+            <img src="images/ico_usuario.png">
+            ${$("#username").val()}
+        </span>
+    `);
+})
+
+ws.on('clientLogin', function (data) {
+    $("#messages")
+    .append(`
+        <div class="dialogo">
+            <h4> ${data.username} </h4>
+            <p> just logged in. </p>
+        </div>
+    `);
+
+    if ($("#" + users).length == 0) {
+        $("#users")
+        .append(`
+            <span class="user" id="${data.id}">
+                <img src="images/ico_usuario.png">
+                ${data.username}
+                </span>
+        `);
+    }
+});
+
+$("#send").click(function () {
+    let message = $("#message-input").val();
+    $("#message-input").val("");
+
+    ws.emit("clientMessage", { username: $("#username").val(), id: socketId, message: message });
+});
+
+ws.on('clientMessage', function (data) {
+    $("#messages")
+    .append(`
+        <div class="dialogo">
+            <h4> ${data.username} </h4>
+            <p> ${data.message} </p>
+        </div>
+    `);
+
+    window.scrollTo(0, document.body.scrollHeight);
+});
+
+ws.on('newConnection', function (data) {
+    usersArray = data.onlineUsers;
+
+    for (let i = 0; i < usersArray.length; i++) {
+        if ($("#" + usersArray[i]).length == 0) {
+            $("#users")
+            .append(`
+                <span class="user" id="${data.id}">
+                    <img src="images/ico_usuario.png">
+                    ${data.name}
+                </span>
+            `);
+        }
+    }
+});
+
+ws.on('userDisconnect', function (data) {
+    $(`#${data.id}`).remove();
+});
